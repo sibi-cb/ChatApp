0c7594a2d1a643be81c4e8430d038126b474551a
Sibi <selamparithi@equinix.com>
2020-09-06 21:38:43 +0530

    initial

---
+++ server/server.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const http = require('http');
+const express = require('express');
+const socketIO = require('socket.io');
+
+const publicPath = path.join(__dirname, '/../public');
+const port = process.env.PORT || 3000;
+
+let app = express();
+let server = http.createServer(app);
+let io = socketIO(server);
+
+app.use(express.static(publicPath));
+
+io.on('connection',(socket) => {
+    console.log('A new user connected');
+
+    socket.on('createMessage', (message) => {
+        console.log("createMessage", message);
+        io.emit('newMessage', {
+            from: message.from,
+            text: message.text,
+            createdAt: new Date().getTime()
+        });
+    });
+
+    socket.on('disconnect',() => {
+        console.log('User disconnected');
+    });
+    
+});
+
+server.listen(port, ()=>{
+    console.log(`Server is up on port ${port}`);
+});
